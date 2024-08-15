275e8ff7b2610a7e82b889fc8853943c9d2e846f
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2024-08-15 15:03:20 -0700

    refetch counter when click

--- frontend/components/Counter.tsx
+++ frontend/components/Counter.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useWallet } from "@aptos-labs/wallet-adapter-react";
 import { useWalletClient } from "@thalalabs/surf/hooks";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 // Internal components
 import { aptosClient, surfClient } from "@/utils/aptosClient";
@@ -12,23 +13,37 @@ export function Counter() {
   const [counter, setCounter] = useState<number>(0);
   const { account } = useWallet();
   const { client: walletClient } = useWalletClient();
+  const queryClient = useQueryClient();
 
-  useEffect(() => {
-    if (!account || !walletClient) {
-      console.error("Account or wallet client not available");
-      return;
-    }
+  const { data } = useQuery({
+    queryKey: ["counter", account?.address],
+    refetchInterval: 10_000,
+    queryFn: async () => {
+      try {
+        const counter = await surfClient()
+          .view.count({
+            typeArguments: [],
+            functionArguments: [account?.address as `0x${string}`],
+          })
+          .then((result) => {
+            return parseInt(result[0]);
+          });
 
-    surfClient()
-      .view.count({
-        typeArguments: [],
-        functionArguments: [account?.address as `0x${string}`],
-      })
-      .then((result) => {
-        setCounter(parseInt(result[0]));
-      })
-      .catch(console.error);
-  }, []);
+        return {
+          counter,
+        };
+      } catch (error: any) {
+        toast({
+          variant: "destructive",
+          title: "Error",
+          description: error,
+        });
+        return {
+          counter: 0,
+        };
+      }
+    },
+  });
 
   const onClickButton = async () => {
     if (!account || !walletClient) {
@@ -44,6 +59,7 @@ export function Counter() {
       const executedTransaction = await aptosClient().waitForTransaction({
         transactionHash: committedTransaction.hash,
       });
+      queryClient.invalidateQueries();
       toast({
         title: "Success",
         description: `Transaction succeeded, hash: ${executedTransaction.hash}`,
@@ -52,6 +68,12 @@ export function Counter() {
       console.error(error);
     }
   };
+
+  useEffect(() => {
+    if (data) {
+      setCounter(data.counter);
+    }
+  }, [data]);
 
   return (
     <div className="flex flex-col gap-6">
